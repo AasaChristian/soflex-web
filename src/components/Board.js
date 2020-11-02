bef7c3f5c453ab543bd5870eeefbe9788b37ea2a
Aasa-Christian <aasachristian@yahoo.com>
2020-11-02 14:31:35 -0500

    addedn component folder to hold components

---
+++ src/components/Board.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+
+function Board() {
+
+    const BoardCont = styled.div`
+    display: flex;
+    height: 100%
+    
+    
+    ;`
+
+    const LeftWall = styled.section`
+    background-color: blue;
+    margin-color: black;
+    width: 12.5%
+
+    
+    `;
+    
+
+    const MainWall = styled.section`
+    background-color: red;
+    width: 75%
+    `;
+
+    const RightWall = styled.section`
+    background-color: yellow;
+    width: 12.5%
+    `;
+
+
+
+
+  return (
+    <BoardCont className="Board">
+
+        <LeftWall>
+            <h2>Left Wall</h2>
+        </LeftWall>
+        <MainWall>
+            <h1>MainWall</h1>
+        </MainWall>
+        <RightWall>
+            <h2>Right Wall</h2>
+        </RightWall>
+      
+    </BoardCont>
+  );
+}
+
+export default Board;
---
+++ src/components/DummyData.js
@@ -0,0 +1,112 @@
+const data = {
+    user: {
+        id: 1,
+
+        Credentials:{
+            userName: 'AceCity',
+            email: 'AasaChristian@yahoo.com',
+            password: 12345
+        },
+
+        Physique:{
+            height: 70,
+            weight: 200,
+            sex: 'male'
+        },
+
+        photo: [],
+
+        ExerciseH: {
+
+            Standard:[
+                {
+                    name: "Bench Press",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "1"
+
+                },
+                {
+                    name: "Squats",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "2"
+                },
+                {
+                    name: "Dead Lifts",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "3"
+                },
+                {
+                    name: "pull ups",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "4"
+                },
+                {
+                    name: "bicep curl",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "5"
+                },
+                {
+                    name: "Bench Press",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "11"
+
+                },
+                {
+                    name: "Squats",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "12"
+                },
+                {
+                    name: "Bench Press",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "121"
+
+                },
+                {
+                    name: "Squats",
+                    weight: 0,
+                    sets: 0,
+                    reps: 0,
+                    key: "122"
+                },
+            ]
+        }
+
+
+    },
+
+    blog: {
+        
+        post:{
+            id:1,
+            userId:1,
+            text: "I'm so exited to track my fitness jurney",
+            photo: null
+        },
+
+        comment: {
+            id:1,
+            userId:1,
+            text: "It's lonely here. lol",
+            photo: null
+        },
+
+    }
+}
+export default data
