0654f6a9be66cf42952932aaf7a538b1d61ba726
Aasa-Christian <aasachristian@yahoo.com>
2020-11-03 16:16:29 -0500

    wrapped appjs in browser router comp

---
+++ src/components/ExerciseDetailPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+function ExDetailsPage() {
+
+  return (
+<div>
+    <h1>ExDetailsPage</h1>
+</div>
+  );
+}
+
+export default ExDetailsPage;
