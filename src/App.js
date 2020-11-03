664da07e489b376b8ba1729ee5450d845f45128b
Aasa-Christian <aasachristian@yahoo.com>
2020-11-03 16:54:20 -0500

    added routs to appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Board from './components/Board';
+import {Route} from "react-router-dom";
 import Head from './components/Head';
+import Board from './components/Board';
+import ExDetailsPage from './components/ExerciseDetailPage';
+import data from './DummyData'
 
 function App() {
+
+  console.log(data, "data")
+  const {user} = data
+  const exlist = user.ExerciseH.Standard
 
 
 
@@ -14,7 +21,12 @@ function App() {
       <header>
         <Head/>
       </header>
-      <Board/>
+      <Route exact path='/board'
+      render={props => <Board {...props} exlist={exlist} />}
+      />
+      <Route path='/board/:exName'
+      render={props => <ExDetailsPage {...props}  exlist={exlist}/>}
+      />
     </div>
   );
 }
