cd5879c441d4c4d5a0d4d6af64249a82236b7f10
Aasa-Christian <aasachristian@yahoo.com>
2020-11-05 18:10:34 -0500

    created a login component

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import {NavLink} from "react-router-dom"
+import axios from 'axios'
+
+
+function Login({history}) {
+
+const [credentials, setCredentials] = useState([{
+    username: '',
+    password: ''
+}])
+
+console.log(credentials)
+const handleChange = e => {
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+}
+
+const login = (e) => {
+    console.log("Button")
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/users/login', {username: credentials.username, password: credentials.password})
+    .then(res => {
+        const {id, username} = res.data
+
+        localStorage.setItem('id', id)
+        localStorage.setItem('username', username)
+        history.push('/board')
+    }).catch(error => console.log(error, "error"))
+}
+
+  return (
+<div>
+<form onSubmit={login}>
+    <input type="text" name="username" onChange={handleChange} placeholder="UserName" />
+    <input type="text" name="password" onChange={handleChange} placeholder="PassWord"/>
+    <button type="submit">LOGIN</button>
+</form> 
+</div>
+  );
+}
+
+export default Login;
