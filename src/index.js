f7ee5199e6d37d8642b19ba5997424d94d783959
Aasa-Christian <aasachristian@yahoo.com>
2020-11-13 21:51:16 -0500

    wrapped app in Provider with redux-thunk and logger

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {reducer} from './reducers'
- 
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+  );
 
 ReactDOM.render(
 <Provider store={store}>
