5d3adbee005b11cf899a72784afc8049accc217a
Aasa-Christian <aasachristian@yahoo.com>
2020-11-20 23:47:56 -0500

    seperated regimen components

---
+++ src/components/RegimenComps/RegimenInpute.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux';
+import { createRegimen, clearTempRegName } from '../../action/regimenActions';
+
+function RegInput(props){
+const {selectedExercise, setSelectedExercise, createRegimen, exercises, compSet, setCompSet, compRep,  setCompRep, compWeight, setCompWeight, setConfEx, chosenExercise, clearTempRegName} = props
+const [newReg, setNewReg] = useState({})
+
+const handleChange = e => {
+    setNewReg({...newReg, [e.target.name]: e.target.value})
+}
+const userId = localStorage.getItem('id')
+
+const sendNewReg = (e) => {
+
+e.preventDefault()
+const createdLink = (chosenExercise.name + userId).toLowerCase().replace(" ", "")
+const newRegimenObj = {
+    name: chosenExercise.name,
+    link: createdLink,
+    userId: userId,
+    exerciseId: selectedExercise,
+    sets: newReg.sets,
+    reps: newReg.reps,
+    weight: newReg.weight,
+    completion: false
+}
+createRegimen(newRegimenObj)
+
+setNewReg({
+    weight: "",
+    reps: "",
+    sets: ""
+})
+setConfEx(false)
+setCompRep(false)
+setCompSet(false)
+setCompWeight(false)
+setSelectedExercise(null)
+clearTempRegName()
+}
+
+const confirmWeight = (e) => {
+    e.preventDefault()
+    setCompWeight(!compWeight) 
+}
+
+const UnWeight = (e) => {
+    e.preventDefault()
+    setConfEx(false)
+    setNewReg({})
+}
+
+const confirmSets = (e) => {
+    e.preventDefault()
+    setCompSet(!compSet) 
+}
+
+const unSets = (e) => {
+    console.log('here')
+    e.preventDefault()
+    setCompWeight(false)
+}
+
+const confirmReps = (e) => {
+    e.preventDefault()
+    setCompRep(!compRep) 
+}
+
+const unReps = (e) => {
+    e.preventDefault()
+    setCompSet(false)
+}
+
+const inputStyle = {display: "flex", height: "200px",  width:"95%", paddingLeft: "30%", marginLeft: "2%", borderRadius: "10%", fontSize:"50px"}
+const displayNone = {display: 'none'}
+const SpacedEven = {display: "flex", justifyContent: 'space-evenly'}
+return(
+<section>
+    <div>
+<p>{newReg.weight}</p>
+<p>{newReg.sets}</p>
+<p>{newReg.reps}</p>
+<p></p>
+    </div>
+    <form onSubmit={sendNewReg}>
+        <div  style={ compWeight === false? {display: 'initial'} : displayNone}>
+        <p>How Much weight?</p>
+        <div style={SpacedEven}>
+
+        <p onClick={UnWeight} >
+        BACK
+        </p>
+        <div onClick={confirmWeight}>
+        <p  >NEXT</p>
+        </div>
+
+        </div>
+
+        </div>
+
+    <input  value={newReg.weight} style={ compWeight === false? inputStyle : displayNone} type="text" name="weight" placeholder="Weight"  onChange={handleChange}/>
+
+    <div style={ compWeight === true && compSet === false? {display: 'initial'} : displayNone}>
+        <p>How Many Sets?</p>
+        <div style={SpacedEven}>
+        <p onClick={unSets}> BACK</p>
+        <p onClick={confirmSets} >NEXT</p>
+        </div>
+        </div>
+
+    <input value={newReg.sets} style={compWeight === true && compSet === false ? inputStyle: displayNone} type="text" name="sets" placeholder="Sets"  onChange={handleChange}/>
+
+    <div  style={ compWeight === true && compSet === true && compRep === false? {display: 'initial'} : displayNone}>
+
+    <p>How Many Reps?</p>
+    <div style={SpacedEven}>
+        <p onClick={unReps}>BACK</p>
+        <p onClick={confirmReps} >NEXT</p>
+        </div>
+        </div>
+
+    <input value={newReg.reps} style={compWeight === true && compSet === true && compRep === false? inputStyle : displayNone} type="text" name="reps" placeholder="Reps"  onChange={handleChange}/>
+
+    <button>Create</button>
+    </form>
+</section>
+
+)
+}
+
+const mapStateToProps = state => {
+	return {
+        exercises: state.exercises
+	};
+};
+export default connect(mapStateToProps, {createRegimen, clearTempRegName})(RegInput);
