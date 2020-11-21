6ebbe298cbcf9974a6bd4689b3adb3668a53cd21
Aasa-Christian <aasachristian@yahoo.com>
2020-11-20 23:47:01 -0500

    created a component and actions to created an temp regimen name

--- src/action/index.js
+++ src/action/index.js
@@ -8,4 +8,8 @@ export const Create_Exercise = 'Create_Exercise'
 
 export const Fetch_Regimen = 'Fetch_Regimen'
 
-export const Create_Regimen = 'Fetch_Regimen'
+export const Create_Regimen = 'Create_Regimen'
+
+export const Create_TempRegName = 'Create_TempRegName'
+
+export const Clear_TempRegName = 'Clear_TempRegName'
