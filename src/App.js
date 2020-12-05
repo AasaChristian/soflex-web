975c6768ecb140d90212bbb6246ceee8be7f0b1d
Aasa-Christian <aasachristian@yahoo.com>
2020-12-05 15:50:42 -0500

    updated footer styling

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
       render={props => <ExForm {...props} axiosAddress={axiosAddress} />  }
       />
     </Switch>
-      <section style={{position: "absolute", bottom: "0", width: "100%"}}>
+      <section style={{position: "fixed", bottom: "0", width: "100%"}}>
         <Foot/>
       </section>
     </div>
