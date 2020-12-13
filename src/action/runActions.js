94b6527dfdd292b49ffd576d0e32884a5ca261c2
Aasa-Christian <aasachristian@yahoo.com>
2020-12-13 01:32:17 -0500

    made fetchlogs axction

--- src/action/runActions.js
+++ src/action/runActions.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
-import {Create_Log} from './index'
+import {Create_Log, Fetch_Logs} from './index'
 
 const axiosAddress = "http://localhost:5000"
+
+export const fetchLogs = (id) => dispatch => {
+    axios.get(`${axiosAddress}/api/logs/find/${id}`)
+    .then(res => {
+        
+         dispatch({type: Fetch_Logs, payload: res.data })
+         
+    })
+    .catch(error => console.log(error, "fetchLogs action error"))
+};
 
 
 export const createLog  = (log) => dispatch => {
