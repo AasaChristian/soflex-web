d3b355140de804d8e5716184e077b1da575f3aa4
Aasa-Christian <aasachristian@yahoo.com>
2020-12-22 19:47:50 -0500

    added existing regimen names to tempregname component, allowes user to create new regimen without retyping reg name

--- src/components/RegimenComps/TempRegName.js
+++ src/components/RegimenComps/TempRegName.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import { createRegimen, createTempRegName } from '../../action/regimenActions';
 
 function TempRegName(props){
-const {createTempRegName, regTempName, setNewRegNameEditor} = props
+const {createTempRegName, regTempName, setNewRegNameEditor, regimenName} = props
 
 const [tempRegName, setTempRegName] = useState("")
+const [nameOfUpdatedReg, setNameOfUpdatedReg] = useState("")
 console.log(tempRegName, "tempRegName")
 console.log(regTempName, 'regTempName Here')
-
+console.log(nameOfUpdatedReg, "nameOfUpdatedReg")
 
 
 const handleChange = e => {
@@ -23,8 +24,27 @@ const submitTempName = e => {
     setNewRegNameEditor(false)
 }
 
+const handleButtonPressed = e => {
+    e.preventDefault()
+}
+const useUpdatedNameAsTemp = e =>{
+    e.preventDefault()
+    setNameOfUpdatedReg()
+}
+
 return(
     <div >
+        {regimenName.map((regimenName) => {
+            return(
+                <div>
+                    <button onClick={(e) => {
+                    e.preventDefault()
+                    createTempRegName(regimenName)   
+                    }}>{regimenName}</button>
+                </div>
+                
+            )
+        })}
     <form style={{display:"flex", flexDirection: "column", justifyContent: "center"}} onSubmit={submitTempName}>
     <input value={tempRegName}  style={{display: "flex", height: "60px", paddingLeft: "40%", marginLeft: "2%", borderRadius: "10%"}} type="text" name="tempRegName" placeholder="Name" onChange={handleChange}/>
     <button>Create</button>
@@ -36,7 +56,8 @@ return(
 
 const mapStateToProps = state => {
 	return {
-        regTempName: state.regTempName
+        regTempName: state.regTempName,
+        regimenName: state.regimenName,
 	};
 };
 export default connect(mapStateToProps, {createRegimen, createTempRegName})(TempRegName);
