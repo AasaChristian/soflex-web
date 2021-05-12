93f13564b65cd1d859bf1407edb9a478d0827861
Aasa-Christian <aasachristian@yahoo.com>
2021-05-11 21:38:44 -0400

    changed landing height to 100%

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,12 +7,12 @@ import Logo from "../img/logo.jpg"
 function Landing() {
 
   return (
-   <section style={{display: "flex", flexDirection: "column", justifyContent: "space-evenly", height: "85%", overflow: 'scroll'}}>
+   <section style={{display: "flex", flexDirection: "column", justifyContent: "space-evenly", height: "100%", overflow: 'scroll'}}>
        <div style={{display: "flex", justifyContent: "center"}}>
            <h1 className="city"> CITY SO FLEX</h1>
        </div>
        
-       <div display={{height: "50px"}}>
+       <div display={{height: "50%"}}>
        <NavLink to={"/login"} style={{display: "flex", justifyContent: "center"}}> 
        <div style={{display: "flex", justifyContent: "center"}}>
            <img src={Logo} alt="picture of weight room"  style={{width: '90%', height: '100%', objectFit: "cover"}}/>
